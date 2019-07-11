89d8b5a31018281084b486b9a6b7e7984ff4f700
MarianoYabrun <52494885+MarianoYabrun@users.noreply.github.com>
2019-07-11 11:24:11 -0300

    Add files via upload

---
+++ 2-InstruccionIf/3-InstruccionSwitch/jsInstruccionSwitch(1).js
@@ -0,0 +1,35 @@
+function mostrar()
+{
+
+var mes = document.getElementById("mes").value;
+// EL break cierra las case. y el default es todo lo que no esta en los case.
+switch(mes)
+{
+case "Enero" :
+
+alert("que comiences bien el año");
+
+break;
+
+case "Marzo" :
+
+alert(" A clases ");
+
+break;
+
+case "Julio" :
+
+alert("Se vienen las Vacaciones");
+
+break;
+
+case "Diciembre" :
+
+alert("Felices fiestas")
+
+break;
+
+}
+
+
+}//FIN DE LA FUNCIÓN
