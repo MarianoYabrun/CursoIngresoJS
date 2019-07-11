1602b5d40d7b5041f5a8d1f93ecf8a4d547c1e87
MarianoYabrun <52494885+MarianoYabrun@users.noreply.github.com>
2019-07-11 11:26:19 -0300

    Add files via upload

--- 3-InstruccionSwitch/jsInstruccionSwitch(2).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(2).js
@@ -1,9 +1,46 @@
 function mostrar()
 {
 //tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
 
-alert (mesDelAño);
+var mes = document.getElementById("mes").value;
+
+switch(mes)
+{
+    case "Enero" :
+
+    case "Febrero" :
+    
+    case "Marzo" :
+    
+    case "Abril" :
+    
+    case "Mayo" :
+    
+    case "Junio" :
+    
+    alert("Falta para el invierno");
+
+     break;
+     
+     case "Julio" :
+
+     case "Agosto" :
+
+     alert("Abrigate que hace frio");
+
+     break;
+
+     default: alert("Ya pasamos el frio, ahora calor!");
+
+}
+
+
+
+
+
+
+
+
 
 
 
