5bada59efe9b1e430295ef3299df739688719f02
MarianoYabrun <52494885+MarianoYabrun@users.noreply.github.com>
2019-07-13 16:08:40 -0300

    Add files via upload

--- 3-InstruccionSwitch/jsInstruccionSwitch(5).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(5).js
@@ -1,10 +1,36 @@
 function mostrar()
 {
-//tomo la edad  
-var laHora = document.getElementById('hora').value;
 
-//alert (laHora);
-	
+var hora = document.getElementById("hora").value ;
+
+switch(hora) 
+{
+case "7":
+
+case "siete" :
+
+case "8" :
+
+case "ocho" :
+
+case "9" :
+
+case "nueve" :
+
+case "10" :
+
+case "diez" :
+
+case "11" :
+
+case "once" :
+
+alert("Es de mañana");
+
+break;
+
+}
+
 	
 
 
